b3e940b83b8d88bb5cb1befc00b5f613fe44ae5a
George Walter Colgrove IV <georgecolgrove@outlook.com>
2016-07-23 20:04:07 -0400

    Start of Resume Website

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
-Hello World
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style/site.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js">
+
+
+        </script>
+    </head>
+    <body>
+        <div id="contentback">
+        </div>
+        <div id="top">
+            <header>
+                <img src="images/personalLogo2.png" width="100" height="100" align="bottom">
+                <br />
+                <strong>George Walter Colgrove IV</strong>
+            </header>
+                <section id="content">
+                    Student of Rochester Institute of Technology
+
+
+                </section>
+        </div>
+        <footer>
+            Copyright George Colgrove
+        </footer>
+    </body>
+</html>
