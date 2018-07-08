0ed838eeb429499183667a5f2a92b66ef64e7765
CaptainGlac1er <georgecolgrove@outlook.com>
2018-07-08 14:18:52 -0500

    changed cdn

--- www/php/template.php
+++ www/php/template.php
@@ -10,7 +10,10 @@ if(!isset($showTemplate) || $showTemplate){ ?>
                }
         ?>
         <link rel="stylesheet" href="/style/site.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js">
+        <script
+            src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E="
+            crossorigin="anonymous">
         </script>
         <link rel="icon"
       type="image/png"
